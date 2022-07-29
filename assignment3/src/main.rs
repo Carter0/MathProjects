2411fa3e8ac2c609b3c518d1c3f626c733faa4c7
carter <weinbergcarter@gmail.com>
2022-07-29 12:45:13 -0400

    Adding the child rect

--- assignment3/src/main.rs
+++ assignment3/src/main.rs
@@ -4,7 +4,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(show_origin)
-        .add_startup_system(add_rectangle)
         .add_startup_system(add_player)
         .add_system(move_player)
         .add_system(rotate_player)
@@ -29,19 +28,6 @@ fn show_origin(mut commands: Commands) {
 #[derive(Component)]
 struct Rect;
 
-fn add_rectangle(mut commands: Commands) {
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(10.0, 10.0)),
-                ..Default::default()
-            },
-            transform: Transform::from_translation(Vec3::new(-50.0, 100.0, 0.0)),
-            ..Default::default()
-        })
-        .insert(Rect);
-}
-
 // The float value is the player movement speed in 'pixels/second'.
 #[derive(Component)]
 struct Player {
@@ -64,6 +50,19 @@ fn add_player(mut commands: Commands) {
             speed: 300.0,
             // degrees per second
             rotation_speed: f32::to_radians(360.0),
+        })
+        .with_children(|player| {
+            // child cube
+            player
+                .spawn_bundle(SpriteBundle {
+                    sprite: Sprite {
+                        custom_size: Some(Vec2::new(10.0, 10.0)),
+                        ..Default::default()
+                    },
+                    transform: Transform::from_translation(Vec3::new(-50.0, 100.0, 0.0)),
+                    ..Default::default()
+                })
+                .insert(Rect);
         });
 }
 
@@ -72,32 +71,31 @@ fn move_player(
     mut player_query: Query<(&Player, &mut Transform)>,
     time: Res<Time>,
 ) {
-    if let Ok((player, mut transform)) = player_query.get_single_mut() {
-        // Get input from the keyboard (WASD)
-        let up: bool = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
-        let down: bool =
-            keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
-        let left: bool =
-            keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
-        let right: bool =
-            keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
+    let (player, mut transform) = player_query
+        .get_single_mut()
+        .expect("Could not find single player");
 
-        // If left is pressed than it will be -1, right 1, both they cancel out.
-        let x_axis: i8 = -(left as i8) + right as i8;
-        let y_axis: i8 = -(down as i8) + up as i8;
-        let move_delta: Vec2 = Vec2::new(x_axis as f32, y_axis as f32);
+    // Get input from the keyboard (WASD)
+    let up: bool = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
+    let down: bool = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
+    let left: bool = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
+    let right: bool = keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
 
-        // move the player
-        let delta_time = time.delta_seconds();
-        transform.translation.x += move_delta.x * player.speed * delta_time;
-        transform.translation.y += move_delta.y * player.speed * delta_time;
-    }
+    // If left is pressed than it will be -1, right 1, both they cancel out.
+    let x_axis: i8 = -(left as i8) + right as i8;
+    let y_axis: i8 = -(down as i8) + up as i8;
+    let move_delta: Vec2 = Vec2::new(x_axis as f32, y_axis as f32);
+
+    // move the player
+    let delta_time = time.delta_seconds();
+    transform.translation.x += move_delta.x * player.speed * delta_time;
+    transform.translation.y += move_delta.y * player.speed * delta_time;
 }
 
 fn rotate_player(
     keyboard_input: Res<Input<KeyCode>>,
     mut player_query: Query<(&Player, &mut Transform)>,
-    time: Res<Time>
+    time: Res<Time>,
 ) {
     let (player, mut transform) = player_query
         .get_single_mut()
@@ -117,10 +115,11 @@ fn rotate_player(
 
     // update the player rotation around the Z axis (perpendicular to the 2D plane of the screen)
     let delta_time = time.delta_seconds();
-    transform.rotate(Quat::from_rotation_z(rotation_factor * player.rotation_speed * delta_time));
+    transform.rotate(Quat::from_rotation_z(
+        rotation_factor * player.rotation_speed * delta_time,
+    ));
 }
 
 // TODO
-// 2. Set the cube to be a child of the player
 // 3. Draw a rect from the origin to the cube and
 //    show the global coordinates of the vector using vector math.
